24562349e192553a91bfdd7ad7272b7022ad3501
Last David Lantican <lastdavid.lantican@gmail.com>
2023-03-21 16:38:16 +0800

    fix conditional rendering to add login, signup, and logout button on home page

--- config/routes.rb
+++ config/routes.rb
@@ -31,5 +31,5 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy', as: :session_logout
+  delete '/logout', to: 'sessions#destroy', as: :logout
 end
