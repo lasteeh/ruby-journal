8fbe3a9d8b42b4b1bbbda1654d8b3586cd1fb20d
Last David Lantican <lastdavid.lantican@gmail.com>
2023-03-22 15:07:53 +0800

    update setting task params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,7 +48,7 @@ class TasksController < ApplicationController
     private
 
     def set_task
-        @task = Task.find(params[:id])
+        @task = current_user.tasks.find(params[:id])
     end
 
     def task_params
