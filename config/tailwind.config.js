5ebe6c5d9f76f8d23b454c2ff430e7ade1fd091f
Last David Lantican <lastdavid.lantican@gmail.com>
2023-03-23 11:53:12 +0800

    style home page, login page, signup page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,24 +1,31 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Lato", "sans-serif"],
+        hand: ["Reenie Beanie", "cursive"],
+      },
+      colors: {
+        primary: {
+          200: "hsl(0,100%,56%)",
+          400: "hsl(0,100%,36%)",
+        },
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
