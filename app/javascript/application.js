3c780e50b056333c1f09b8cb6a50f1c97e7b90e0
Last David Lantican <lastdavid.lantican@gmail.com>
2023-03-27 15:51:40 +0800

    fix resource thing

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,35 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
+
+window.onload = function () {
+  const categoryLinks = document.querySelectorAll(
+    "#paperlinks [data-category]"
+  );
+  if (categoryLinks) {
+    console.log("hi");
+    categoryLinks.forEach(function (link) {
+      link.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        categoryLinks.forEach(function (link) {
+          link.classList.remove("active");
+        });
+        this.classList.add("active");
+
+        const categoryId = this.dataset.category;
+        const taskLists = document.querySelectorAll(
+          "#papertasks [data-category]"
+        );
+
+        taskLists.forEach(function (list) {
+          if (categoryId == "all" || list.dataset.category == categoryId) {
+            list.style.display = "block";
+          } else {
+            list.style.display = "none";
+          }
+        });
+      });
+    });
+  }
+};
