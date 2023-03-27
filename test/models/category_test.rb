0cadafef528d81d9d1498e10d33f022c8717e03a
Last David Lantican <lastdavid.lantican@gmail.com>
2023-03-27 23:38:52 +0800

    tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,42 +4,25 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @category = categories(:category1)
+  end
 
-  # def setup
-  #   @category = categories(:one)
-  #   @user = users(:one)
-  # end
+  test "should be valid" do
+    assert @category.valid?
+  end
 
-  # test "should be valid" do
-  #   assert @category.valid?
-  # end
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
 
-  # test "name should be present" do
-  #   @category.name = "    "
-  #   assert_not @category.valid?
-  # end
+  test "user_id should be present" do
+    @category.user_id = nil
+    assert_not @category.valid?
+  end
 
-  # test "name should be unique" do
-  #   duplicate_category = @category.dup
-  #   @category.save
-  #   assert_not duplicate_category.valid?
-  # end
-
-  # test "name should be downcased before validation" do
-  #   @category.name = "CATEGORY"
-  #   @category.save
-  #   assert_equal "category", @category.name
-  # end
-
-  # test "should belong to a user" do
-  #   assert_not_nil @category.user
-  # end
-
-  # test "should have many task categories" do
-  #   assert_instance_of TaskCategory, @category.task_categories.build
-  # end
-
-  # test "should have many tasks through task categories" do
-  #   assert_instance_of Task, @category.tasks.build
-  # end
+  test "favorite should default to false" do
+    assert_not @category.favorite?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,17 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  # def setup
+  #   @user = User.new(username: "testuser", email: "testuser@example.com", password: "password")
+  # end
+
+  # test "password should be encrypted" do
+  #   assert_not_equal @user.password, "password"
+  #   assert BCrypt::Password.new(@user.password) == "password"
+  # end
+
+  # test "password should be encrypted" do
+  #   assert_not_equal @user.password, "password"
+  #   assert Password.create(@user.password) == "password"
+  # end
 end
